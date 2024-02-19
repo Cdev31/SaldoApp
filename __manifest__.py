c95847867f05c873c4683a9d0c8a97578b102992
kalet chavez <kalet.svdev@gmail.com>
2024-02-18 23:55:48 -0600

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Saldo App",
+    "description": "Aplicacion web para registart ingresos y egresos",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        
+    ]
+}
